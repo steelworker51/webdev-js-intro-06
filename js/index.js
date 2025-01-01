e79c7bef70a669be1cefa002c7ec46637a71fcc5
dale murphy <smcpsycho51@gmail.com>
2025-01-01 17:03:32 -0500

    changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*for (let i = 0; i < 10; i++) {
+    if (i === 5) {
+        break;  
+    }
+    console.log(i);
+}*/
+
+/*let i = 0;
+while (i < 10) {
+    if (i === 5) {
+        break;  
+    }
+    console.log(i);
+    i++;
+}*/
+
+/*let i = 0;
+do {
+    if (i === 5) {
+        break;  // Exit the loop when i is 5
+    }
+    console.log(i);
+    i++;
+} while (i < 10);*/
